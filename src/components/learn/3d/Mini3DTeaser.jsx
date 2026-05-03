9948bee45d1b601429861cd4e5a39cdae573ec9e
marksmulders1973 <marksmulders1973@gmail.com>
2026-05-03 20:10:06 +0200

    Mini3DTeaser: camera nog verder (2.9 -> 3.6) zodat kubus-diagonaal niet boven canvas uitsteekt tijdens rotatie

--- src/components/learn/3d/Mini3DTeaser.jsx
+++ src/components/learn/3d/Mini3DTeaser.jsx
@@ -46,7 +46,7 @@ export default function Mini3DTeaser({ onCTA }) {
           dimensions={{ zijde: z }}
           labels={[]}
           height={140}
-          cameraDistanceFactor={2.9}
+          cameraDistanceFactor={3.6}
         />
       </div>
       <div style={{ display: "flex", alignItems: "center", gap: 10, marginBottom: 6 }}>
